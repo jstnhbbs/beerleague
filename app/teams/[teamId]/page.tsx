701e32ee669cc3b04271129cf074b42445105210
justin.hobbs <justinhobbs@fastmail.com>
2026-01-20 14:10:01 -0600

    updating test spreadsheets

--- app/teams/[teamId]/page.tsx
+++ app/teams/[teamId]/page.tsx
@@ -3,10 +3,10 @@ import './team.css'
 
 // Team configuration - you can update these with actual Google Sheet URLs
 const teamSheets: Record<string, string> = {
-    '1': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
-    '2': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
-    '3': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
-    '4': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
+    '1': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?gid=1978093567&single=true',
+    '2': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?gid=1100028925&single=true',
+    '3': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?gid=581855790&single=true',
+    '4': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?gid=505574005&single=true',
     '5': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
     '6': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
     '7': 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ31Kptl6d-ElO2JuEQajDZZt18AGtCcxJ0fKSjEdOYuHb5cyqXFNaj5rG1YGVNt3kScTWtXyIFBmif/pubhtml?widget=true&headers=false',
@@ -34,12 +34,28 @@ interface TeamPageProps {
     }
 }
 
-// Generate static params for all teams at build time
-export function generateStaticParams() {
-    return Array.from({ length: 10 }, (_, i) => ({
-        teamId: String(i + 1),
-    }))
-}
+// /teams/[teamId]/page.js
+
+export async function generateStaticParams() {
+    // Replace with logic to retrieve your team IDs, e.g., from a database or data file
+    const teams = [{ teamId: '1' }, { teamId: '2' }]; // Example
+  
+    return teams.map(team => ({
+      teamId: team.teamId,
+    }));
+  }
+  
+//   // Your regular page component below
+//   export default function TeamPage({ params }) {
+//     // ...
+//   }
+
+// // Generate static params for all teams at build time
+// export function generateStaticParams() {
+//     return Array.from({ length: 10 }, (_, i) => ({
+//         teamId: String(i + 1),
+//     }))
+// }
 
 export default function TeamPage({ params }: TeamPageProps) {
     const { teamId } = params
